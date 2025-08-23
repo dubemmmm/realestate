3f86e29fe698ac409ff3c4d0755e9c07e3b1bc76
Chidubem Onwuchuluba <chidubemonwuchuluba@Chidubems-MacBook-Air.local>
2025-08-23 18:42:33 -0400

    commit

--- realestate/settings.py
+++ realestate/settings.py
@@ -86,10 +86,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 # Password validation
